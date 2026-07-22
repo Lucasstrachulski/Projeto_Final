ccaf4dea315953031a9178c36d3ac83873cbafc2
lucasstrachulski <lucasvstra@gmail.com>
2026-07-22 16:59:58 -0300

    feat: atualiza controller do aluno com integracao do gerador de RA

--- Projeto_Final/src/controllers/aluno.controller.js
+++ Projeto_Final/src/controllers/aluno.controller.js
@@ -1,3 +1,32 @@
-//CRIAR UM CLUSTER NO MONGODB, NOME "PROJETO_FINAL"
-//FELIPE CHAMAR NÓS PARA O REPOSITORIO ORIGINAL
+import AlunoRepository from "../repositories/aluno.repository.js";
+import GerarRA from "../utils/gerarRA.js";
+import bcrypt from "bcryptjs";
 
+export const criarAluno = async (req, res) => {
+  try {
+    const { nome, email, senha, curso } = req.body;
+
+    const alunoExistente = await AlunoRepository.buscarPorEmail(email);
+
+    if (alunoExistente) {
+        return res.status(400).json({ mensagem: "E-mail já cadastrado no sistema." });
+    }
+
+    const ra = await GerarRA();
+
+    const senhaCriptografada = await bycript.hash(senha, 10)
+
+    const novoAluno = await AlunoRepository.criar({
+        nome,
+        email,
+        senha: senhaCriptografada,
+        curso,
+        ra,
+    })
+
+    return res.status(201).json({mensagem:"Aluno cadastrado com sucesso!", aluno: novoAluno})
+
+  } catch (error) {
+    return res.status(500).json({mensagem: "Erro interno do servidor ao cadastrar aluno.",erro: error.message});
+  }
+};
